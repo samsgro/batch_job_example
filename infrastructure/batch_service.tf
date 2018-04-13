7608fb8a03ef47465526a9179c5813977c37427b
Berenice Cotero <berenice.cotero@clarivate.com>
2018-04-13 19:41:41 -0400

    added job queue

--- infrastructure/batch_service.tf
+++ infrastructure/batch_service.tf
@@ -5,9 +5,9 @@ resource "aws_batch_compute_environment" "batch_service" {
  compute_resources {
    instance_role = "${aws_iam_instance_profile.instance_role.arn}"
    instance_type = [
-     "c4.large"
+     "m3"
    ]
-   max_vcpus          = 16
+   max_vcpus          = 3
    min_vcpus          = 1
    desired_vcpus      = 1
    security_group_ids = [
@@ -19,5 +19,12 @@ resource "aws_batch_compute_environment" "batch_service" {
  service_role = "${aws_iam_role.aws_batch_service_role.arn}"
  type = "MANAGED"
  depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
+}
+
+resource "aws_batch_job_queue" "batch_service_wos" {
+ name = "batch-${var.app}-job-queue-${var.user}"
+ state = "ENABLED"
+ priority = 1
+ compute_environments = ["${aws_batch_compute_environment.batch_service.arn}"]
 }
 

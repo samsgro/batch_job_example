3e2cdbcf9b7b1a14dce0f09649e8a113939058bd
Berenice Cotero <berenice.cotero@clarivate.com>
2018-05-04 17:27:59 -0400

    add registry for new Docker image

--- infrastructure/dev/global/registry.tf
+++ infrastructure/dev/global/registry.tf
@@ -7,6 +7,13 @@ resource "aws_ecr_repository" "wos_dev_repository"{
   name = "${var.role}_${var.app}_${var.env}-${var.user}_hello-world"
 }
 
-output "ecr_arn" {
+resource "aws_ecr_repository" "unzip_save_to_s3" {
+  name = "${var.role}_${var.app}_${var.env}-${var.user}_unzip_save_to_s3"
+}
+
+output "ecr_hello_arn" {
   value = "${aws_ecr_repository.wos_dev_repository.arn}"
+}
+output "ecr_unzip_arn"{
+  value = "${aws_ecr_repository.unzip_save_to_s3.arn}"
 }

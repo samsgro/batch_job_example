8d779423e71113a26b01e43565d51bb87c7088bf
Berenice Cotero <berenice.cotero@clarivate.com>
2018-05-10 19:16:26 -0400

    fix gramar in diagram and added resgistry outputs

--- infrastructure/dev/global/registry.tf
+++ infrastructure/dev/global/registry.tf
@@ -14,6 +14,18 @@ resource "aws_ecr_repository" "unzip_save_to_s3" {
 output "ecr_hello_arn" {
   value = "${aws_ecr_repository.wos_dev_repository.arn}"
 }
+output "ecr_unzip_name"{
+  value = "${aws_ecr_repository.unzip_save_to_s3.name}"
+}
+
 output "ecr_unzip_arn"{
   value = "${aws_ecr_repository.unzip_save_to_s3.arn}"
 }
+output "ecr_unzip_registry_id"{
+  value = "${aws_ecr_repository.unzip_save_to_s3.registry_id}"
+}
+
+output "ecr_unzip_repository_url"{
+  value = "${aws_ecr_repository.unzip_save_to_s3.repository_url}"
+}
+

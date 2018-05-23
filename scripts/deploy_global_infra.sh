062e1d18d624e6425f5fab5940646304ed3028b6
Berenice Cotero <berenice.cotero@clarivate.com>
2018-05-23 10:27:57 -0400

    script exit 1 on error--typo

--- scripts/deploy_global_infra.sh
+++ scripts/deploy_global_infra.sh
@@ -5,19 +5,19 @@ echo "deploying global insfrastructure"
 cd infrastructure/dev/global
 terraform init
 
-if [ "$?" -ne 0]; then
+if [ "$?" -ne 0 ]; then
    exit 1
 fi
 
 terraform plan -out terraformplan.out
 
-if [ "$?" -ne 0]; then
+if [ "$?" -ne 0] ; then
    exit 1
 fi
 
 terraform apply terraformplan.out
 
-if [ "$?" -ne 0]; then
+if [ "$?" -ne 0 ]; then
    exit 1
 fi
 

140d95200ce5c2c93903efede68a0dc2741ca045
Berenice Cotero <berenice.cotero@clarivate.com>
2018-05-23 10:24:43 -0400

    script exit 1 on error

--- scripts/deploy_global_infra.sh
+++ scripts/deploy_global_infra.sh
@@ -4,9 +4,23 @@ echo "deploying global insfrastructure"
 
 cd infrastructure/dev/global
 terraform init
-echo $?
+
+if [ "$?" -ne 0]; then
+   exit 1
+fi
+
 terraform plan -out terraformplan.out
+
+if [ "$?" -ne 0]; then
+   exit 1
+fi
+
 terraform apply terraformplan.out
+
+if [ "$?" -ne 0]; then
+   exit 1
+fi
+
 rm -rf terraformplan.out
 
 echo "Consumatum est..."
